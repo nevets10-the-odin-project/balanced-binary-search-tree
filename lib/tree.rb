f1a8ceeea99218c8bcdf16ae872e7bab0d415bf7
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-31 19:02:27 -0800

    Correct data val in build_tree, add pretty_print

--- lib/tree.rb
+++ lib/tree.rb
@@ -12,10 +12,16 @@ class Tree
 
     mid = start + ((ending - start) / 2)
 
-    new_root = Node.new(mid)
+    new_root = Node.new(arr[mid])
     new_root.left_c = build_tree(arr, start, mid - 1)
     new_root.right_c = build_tree(arr, mid + 1, ending)
 
     new_root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_c, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_c
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left_c, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_c
   end
 end
