a21cec16334d2068cd67d6bc667d75ecef04bb1f
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-31 18:59:13 -0800

    Populate built_tree, require node

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,10 +1,21 @@
+require_relative 'node'
+
 class Tree
   attr_accessor :root
 
   def initialize(arr)
-    self.root = build_tree(arr)
+    self.root = build_tree(arr.sort.uniq, 0, arr.sort.uniq.length - 1)
   end
 
-  def build_tree
+  def build_tree(arr, start, ending)
+    return if start > ending
+
+    mid = start + ((ending - start) / 2)
+
+    new_root = Node.new(mid)
+    new_root.left_c = build_tree(arr, start, mid - 1)
+    new_root.right_c = build_tree(arr, mid + 1, ending)
+
+    new_root
   end
 end
