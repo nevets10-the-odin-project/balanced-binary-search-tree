0d6f49d2a4494b2c1037c699ff133e4449dc59ff
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-02 18:27:21 -0800

    Setup demonstration in main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -2,8 +2,34 @@ require_relative 'tree'
 
 arr = (Array.new(15) { rand(1..100) })
 
+def puts_info(tree)
+  tree.pretty_print
+  puts "Is the tree balanced? Survey says #{tree.balanced?}."
+
+  puts 'Level order'
+  p tree.level_order
+
+  puts 'Preorder'
+  p tree.preorder
+
+  puts 'Postorder'
+  p tree.postorder
+
+  puts 'Inorder'
+  p tree.inorder
+end
+
 tree = Tree.new(arr)
+puts_info(tree)
+
+puts '----------------------------------------------------'
+puts "Let's add a few more numbers."
+tree.insert(101)
+tree.insert(102)
+tree.insert(103)
 tree.pretty_print
-puts "The tree is balanced. Survey says #{tree.balanced?}"
-puts 'Level order'
-p tree.level_order
+puts "Is the tree balanced? Survey says #{tree.balanced?}."
+puts '----------------------------------------------------'
+puts "Let's rebalance the tree!"
+tree.rebalance
+puts_info(tree)
